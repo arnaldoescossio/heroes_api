d38772fd971807608efab9b9459186f542753c8e
Arnaldo Escóssio <arnaldoescossio@entetecnologia.com.br>
2021-05-05 21:48:09 -0300

    Consulta por nome

--- app/models/hero.rb
+++ app/models/hero.rb
@@ -1,5 +1,6 @@
 class Hero < ApplicationRecord
     validates :name, presence: true
 
+    scope :search, ->(nome) { where('name LIKE ?', "%#{nome}%") if nome.present? }
     scope :sorted_by_name, -> { order(:name) }
 end
